4f14d1489577125f946c7ecf29f6f84f9bc1dae0
Alfin <alfingamping@gmail.com>
2025-05-27 21:10:10 +0700

    Hari 2 Minggu 2: Dasar Widget dan Layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello World App'),
+          title: Text('Hari 2: Belajar Widget'),
         ),
         body: Center(
-          child: Text(
-            'Hello, Alfin! Selamat datang di Flutter!',
-            style: TextStyle(fontSize: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.blue,
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Halo, Alfin!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                color: Colors.green,
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Ini Container kedua!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
