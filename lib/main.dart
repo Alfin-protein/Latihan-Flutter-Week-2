2c17bb35829a9e45c412f613807534ef11b4f631
Alfin <alfingamping@gmail.com>
2025-05-27 20:11:12 +0700

    Hari 1 Minggu 2: Hello World Flutter

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World App'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, Alfin! Selamat datang di Flutter!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
