0e7bedc61c851a579218ca769f854dd696bb2129
Alfin <alfingamping@gmail.com>
2025-05-28 20:50:01 +0700

    Hari 3 Minggu 2: Interaksi dengan ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -8,39 +8,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hari 2: Belajar Widget'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.blue,
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'Halo, Alfin!',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
+      home: CounterApp(),
+    );
+  }
+}
+
+class CounterApp extends StatefulWidget {
+  @override
+  _CounterAppState createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  int counter = 0;
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hari 3: Counter Interaktif'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Tombol sudah ditekan:',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              '$counter kali',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: incrementCounter,
+              child: Text('Tambah'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               ),
-              SizedBox(height: 20),
-              Container(
-                color: Colors.green,
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'Ini Container kedua!',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
